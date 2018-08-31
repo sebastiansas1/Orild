4b1189a757d7576d1df1b52da79637f3876f8ac9
Sebastian <sebastiansas1@outlook.com>
2018-08-31 16:02:37 +0100

    Added Admin Security

--- routes/users.js
+++ routes/users.js
@@ -7,12 +7,12 @@ const passport = require('passport');
 let User = require('../models/user');
 
 // Register Form
-router.get('/register', function (req, res) {
+router.get('/register', ensureIsAdmin, function(req, res) {
   res.render('user/register');
 });
 
 // Register Process POST
-router.post('/register', function (req, res) {
+router.post('/register', ensureIsAdmin, function(req, res) {
   const name = req.body.name;
   const email = req.body.email;
   const username = req.body.username;
@@ -21,10 +21,17 @@ router.post('/register', function (req, res) {
 
   req.checkBody('name', 'Numele este obligatoriu.').notEmpty();
   req.checkBody('email', 'Email-ul este obligatoriu.').notEmpty();
-  req.checkBody('email', 'Email-ul nu a fost scris intre un format corect.').isEmail();
+  req
+    .checkBody('email', 'Email-ul nu a fost scris intre un format corect.')
+    .isEmail();
   req.checkBody('username', 'Alegeti un username.').notEmpty();
   req.checkBody('password', 'Password-ul este obligatoriu').notEmpty();
-  req.checkBody('password2', 'Password-ul nu a fost confirmat corect. Trebe sa scrieti acelasi parola in ambele sectii.').equals(req.body.password);
+  req
+    .checkBody(
+      'password2',
+      'Password-ul nu a fost confirmat corect. Trebe sa scrieti acelasi parola in ambele sectii.'
+    )
+    .equals(req.body.password);
 
   let errors = req.validationErrors();
 
@@ -40,18 +47,21 @@ router.post('/register', function (req, res) {
       password: password
     });
 
-    bcrypt.genSalt(10, function (err, salt) {
-      bcrypt.hash(newUser.password, salt, function (err, hash) {
+    bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(newUser.password, salt, function(err, hash) {
         if (err) {
           console.log(err);
         }
         newUser.password = hash;
         newUser.save(function(err) {
-          if(err) {
+          if (err) {
             console.log(err);
             return;
           } else {
-            req.flash('success', 'Sunteti inregistrati. Acum puteti efectua login-ul.');
+            req.flash(
+              'success',
+              'Sunteti inregistrati. Acum puteti efectua login-ul.'
+            );
             res.redirect('/users/login');
           }
         });
@@ -67,10 +77,9 @@ router.get('/login', function(req, res) {
 
 // Login Process POST
 router.post('/login', function(req, res, next) {
-
   passport.authenticate('local', {
     successRedirect: '/',
-    failureRedirect: '/users/login', 
+    failureRedirect: '/users/login',
     failureFlash: true
   })(req, res, next);
 });
@@ -80,5 +89,15 @@ router.get('/logout', function(req, res) {
   req.flash('success', 'V-ati delogat cu succes.');
   res.redirect('/users/login');
 });
+
+// Access Control
+function ensureIsAdmin(req, res, next) {
+  if (req.user.username == 'Admin') {
+    return next();
+  } else {
+    req.flash('info', 'Nu puteti accesa aceasta pagina.');
+    res.redirect('/');
+  }
+}
 
 module.exports = router;
